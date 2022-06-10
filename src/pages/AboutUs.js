7b3881247204f70296054996bfef93603b44ecd8
artebleker <artebleker@gmail.com>
2022-06-09 22:39:56 -0300

    about us

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import './aboutUs.css'
+import { testText } from "../test/test";
 const AboutUs = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const nosotros = testText.find((f) => f.name === "Nosotros");
 
-export default AboutUs
+  return (
+    <div className="about-us">
+      <h1>Nosotros</h1>
+      <div className="about-us-grid">
+        <p className="text-aboutus1">{nosotros.text1}</p>
+        <img className="product photo-aboutus1" src={nosotros.photo1} alt="nosotros" />
+        <img className="product photo-aboutus2" src={nosotros.photo2} alt="nosotros" />
+        <p className="text-aboutus2">{nosotros.text2}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
