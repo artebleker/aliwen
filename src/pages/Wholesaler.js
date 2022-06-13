eaa70182bdf3f9a603421495194f9b934b82fd7c
artebleker <artebleker@gmail.com>
2022-06-13 16:33:25 -0300

    mayorista

--- src/pages/Wholesaler.js
+++ src/pages/Wholesaler.js
@@ -1,10 +1,17 @@
 import React from 'react'
-
+import { testText } from '../test/test'
 const Wholesaler = () => {
+
+  const mayorista = testText.find((m)=> m.name === 'Mayorista')
+
   return (
+    <>
     <div>
-      
+      <h1>{mayorista.name}</h1>
+      <p>{mayorista.text}</p>
     </div>
+   
+    </>
   )
 }
 
