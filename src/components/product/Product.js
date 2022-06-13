5017cc924e891ef51b8ebba6d6f67667bff740b2
artebleker <artebleker@gmail.com>
2022-06-13 12:27:06 -0300

    catalogue page

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './product.css'
 
 const Product = (productData) => {
 
   return (
     <div className='container container-fluid'>
-      <img src={productData.productData.photo} alt={productData.productData.name} className='product img-fluid'></img>
+     <Link to={`/producto/${productData.productData.path}`}>
+       <img src={productData.productData.photo} alt={productData.productData.name} className='product img-fluid'></img>
+     </Link>
       <h4>{productData.productData.name}</h4>
       <h5>${productData.productData.price}</h5>
     </div>
+   
   )
 }
 
--- src/pages/Catalogue.js
+++ src/pages/Catalogue.js
@@ -2,6 +2,7 @@ import React from "react";
 import { testProducts, types } from "../test/test";
 import { useParams } from "react-router-dom";
 import Product from "../components/product/Product";
+import "./catalogue.css";
 const Catalogue = () => {
   const { category } = useParams();
 
@@ -9,18 +10,23 @@ const Catalogue = () => {
     category === undefined
       ? testProducts
       : testProducts.filter(
-          (fil) => fil.type === types.find((fin) => fin.category === category).id)
-       
-  console.log(categoryProducts);
-  return( 
-  <div>
-    <h1>{category === undefined? 'Catalogo' : types.find((fin) => fin.category === category).type}</h1>
-    {categoryProducts.map((cat, index)=>{
-      return(
-      <Product key={index} productData={cat}/>
-      )
-    })}
-  </div>
+          (fil) =>
+            fil.type === types.find((fin) => fin.category === category).id
+        );
+
+  return (
+    <div className="catalogue-container">
+      <h1>
+        {category === undefined
+          ? "Catalogo"
+          : types.find((fin) => fin.category === category).type}
+      </h1>
+      <div className="catalogue-products">
+        {categoryProducts.map((cat, index) => {
+          return <Product key={index} productData={cat} />;
+        })}
+      </div>
+    </div>
   );
 };
 
