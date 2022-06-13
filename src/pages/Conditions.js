ab14987e1a1ab53f8b7c3f34b6ac3ef8832693c8
artebleker <artebleker@gmail.com>
2022-06-13 12:59:53 -0300

    terminos y condiciones

--- src/pages/Conditions.js
+++ src/pages/Conditions.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { testText } from '../test/test'
+import './conditions.css'
 const Conditions = () => {
+  const termsAndConditions = testText.find((f)=>f.name === 'Terminos y Condiciones')
   return (
-    <div>
-      
+    <div className='conditions container container-fluid'>
+      <h1>{termsAndConditions.name}</h1>
+      <p>{termsAndConditions.text}</p>
     </div>
   )
 }
