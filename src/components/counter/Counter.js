e7ce7679f3c4bee1392c250c6b6a69501d87ed47
artebleker <artebleker@gmail.com>
2022-06-14 12:11:24 -0300

    detail

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Counter = () => {
   return (
-    <div>Counter</div>
+    <div>
+      
+    </div>
   )
 }
 
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Counter from '../components/counter/Counter'
+import Display from '../components/display/Display'
 
-const Detail = () => {
+const Detail = ({product}) => {
   return (
     <div>
-      
+      <h1>{product.name}</h1>
+      <div className='product-card'>
+        <div className='name-pic-price'>
+          {product.subname&&
+          <h2>{product.subname}</h2>}
+          <img src={product.photo} alt={product.name}/>
+          <p>${product.price}</p>
+        </div>
+        <div className='description-counter'>
+          <p>{product.description}</p>
+          <ul>
+            {product.highlights.map((m, index)=>{
+              return(
+                <li key={index}>{m}</li>
+              )
+            })}
+          </ul>
+          <Counter/>
+          <Display/>
+          <button>Agregar al carrito</button>
+        </div>
+      </div>
     </div>
   )
 }
